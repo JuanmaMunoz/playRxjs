38102cc0b44f53910b3a2a35ac5640b6fc193cd8
Juanma Muñoz Gonzalez <jmmugoz@gmail.com>
2024-12-26 11:40:13 +0100

    combination finished

--- src/app/pages/combination/combination.component.ts
+++ src/app/pages/combination/combination.component.ts
@@ -1,7 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, OnInit, Renderer2 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest, of, Subscription } from 'rxjs';
+import {
+  combineLatest,
+  concat,
+  concatAll,
+  delay,
+  exhaust,
+  forkJoin,
+  map,
+  merge,
+  mergeAll,
+  of,
+  race,
+  startWith,
+  Subscription,
+  switchAll,
+  withLatestFrom,
+} from 'rxjs';
 import { ExampleComponent } from '../../components/example/example.component';
 import { combinations } from '../../info/combinations';
 import { IntroductionComponent } from '../../introduction/introduction.component';
@@ -39,6 +55,16 @@ export class CombinationComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.operatorCombineLatest();
+    this.operatorConcat();
+    this.operatorConcatAll();
+    this.operatorExhaust();
+    this.operatorForkJoin();
+    this.operatorMerge();
+    this.operatorMergeAll();
+    this.operatorRace();
+    this.operatorStartWith();
+    this.operatorSwitchAll();
+    this.operatorwithLatestFrom();
   }
 
   private operatorCombineLatest(): void {
@@ -49,6 +75,125 @@ export class CombinationComponent implements OnInit, AfterViewInit {
       combineLatest([obs1$, obs2$, obs3$]).subscribe((data) =>
         this.addConsole('combineLatest', JSON.stringify(data)),
       ),
+    );
+  }
+
+  private operatorConcat(): void {
+    const obs1$ = of(1, 2).pipe(delay(2000));
+    const obs2$ = of('a', 'b');
+    const obs3$ = of(3, 4, 5);
+    this.subscription.add(
+      concat(obs1$, obs2$, obs3$).subscribe((data) =>
+        this.addConsole('concat', JSON.stringify(data)),
+      ),
+    );
+  }
+
+  private operatorConcatAll(): void {
+    const obs1$ = of(1, 2).pipe(delay(2000));
+    const obs2$ = of('a', 'b');
+    const obs3$ = of(3, 4, 5);
+    const obsAll$ = of(obs1$, obs2$, obs3$);
+    this.subscription.add(
+      obsAll$
+        .pipe(
+          map((obs) => obs), // We transform the values into observables
+          concatAll(), // Concatenates the values of the emitted observables
+        )
+        .subscribe((data) => this.addConsole('concatAll', JSON.stringify(data))),
+    );
+  }
+
+  private operatorExhaust(): void {
+    const obs1$ = of(1, 2);
+    const obs2$ = of('a', 'b').pipe(delay(3000));
+    const obs3$ = of(3, 4, 5);
+    const obsAll$ = of(obs1$, obs2$, obs3$);
+    this.subscription.add(
+      obsAll$.pipe(exhaust()).subscribe((data) => this.addConsole('exhaust', JSON.stringify(data))),
+    );
+  }
+
+  private operatorForkJoin(): void {
+    const obs1$ = of(1, 2).pipe(delay(2000));
+    const obs2$ = of('a', 'b');
+    const obs3$ = of(3, 4, 5);
+    this.subscription.add(
+      forkJoin(obs1$, obs2$, obs3$).subscribe((data) =>
+        this.addConsole('forkJoin', JSON.stringify(data)),
+      ),
+    );
+  }
+
+  private operatorMerge(): void {
+    const obs1$ = of(1, 2).pipe(delay(2000));
+    const obs2$ = of('a', 'b');
+    const obs3$ = of(3, 4, 5);
+    this.subscription.add(
+      merge(obs1$, obs2$, obs3$).subscribe((data) =>
+        this.addConsole('merge', JSON.stringify(data)),
+      ),
+    );
+  }
+
+  private operatorMergeAll(): void {
+    const obs1$ = of(1, 2).pipe(delay(2000));
+    const obs2$ = of('a', 'b');
+    const obs3$ = of(3, 4, 5);
+    const obsAll$ = of(obs1$, obs2$, obs3$);
+    this.subscription.add(
+      obsAll$
+        .pipe(
+          map((obs) => obs), // We transform the values into observables
+          mergeAll(), // Concatenates the values of the emitted observables
+        )
+        .subscribe((data) => this.addConsole('mergeAll', JSON.stringify(data))),
+    );
+  }
+
+  private operatorRace(): void {
+    const obs1$ = of(1, 2).pipe(delay(2000));
+    const obs2$ = of('a', 'b');
+    const obs3$ = of(3, 4, 5);
+    this.subscription.add(
+      race(obs1$, obs2$, obs3$).subscribe((data) => this.addConsole('race', JSON.stringify(data))),
+    );
+  }
+
+  private operatorStartWith(): void {
+    const obs1$ = of(1, 2).pipe(delay(2000));
+    this.subscription.add(
+      obs1$
+        .pipe(startWith(0))
+        .subscribe((data: number) => this.addConsole('startWith', data.toString())),
+    );
+  }
+
+  private operatorSwitchAll(): void {
+    const obs1$ = of(1, 2).pipe(delay(2000));
+    const obs2$ = of('a', 'b');
+    const obs3$ = of(3, 4, 5);
+    const obsAll$ = of(obs1$, obs2$, obs3$);
+    this.subscription.add(
+      obsAll$
+        .pipe(
+          map((obs) => obs), // We transform the values into observables
+          switchAll(), // Concatenates the values of the emitted observables
+        )
+        .subscribe((data) => this.addConsole('switchAll', JSON.stringify(data))),
+    );
+  }
+
+  private operatorwithLatestFrom(): void {
+    const obs1$ = of(1, 2);
+    const obs2$ = of('a', 'b');
+    this.subscription.add(
+      obs1$
+        .pipe(
+          withLatestFrom(obs2$),
+          map(([obs1, obs2]) => obs1 + ' - ' + obs2),
+        )
+        .subscribe((data) => this.addConsole('withLatestFrom', JSON.stringify(data))),
     );
   }
 
