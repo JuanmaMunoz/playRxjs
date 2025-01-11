08deb8acacf35d3c61e35ca4daf612d9eaa8c12d
Juanma Muñoz Gonzalez <jmmugoz@gmail.com>
2025-01-11 21:54:04 +0100

    real life examples

--- src/app/info/realLife.ts
+++ src/app/info/realLife.ts
@@ -23,5 +23,44 @@ export const realLife: IInfo = {
 .subscribe((users: IUser[]) =>
   console.log(users))`,
     },
+    {
+      id: 'mergeUsersHobbiesforkJoinConcatMap',
+      code: `// users = [{"name": "Jhon","firstName": "Trump","age": 40,"job": "Front end developer","salary": 30000},...]
+// hobbies = [{"name": "Jhon","hobbies": ["fishing, music, sport"]},...]
+const users$ = this.userService.getUsers().pipe(delay(2000));
+const hobbies$ = this.userService.gethobbies();
+forkJoin(users$, hobbies$)
+  .pipe(
+    concatMap(([u, h]: [IUser[], IHobby[]]) => {
+      return of({ users: u, hobbies: h }).pipe(
+        map((data: { users: IUser[]; hobbies: IHobby[] }) =>
+          data.users.map((user: IUser) => ({
+            ...user,
+            hobbies: data.hobbies.find((hobby: IHobby) => hobby.name === user.name)?.hobbies,
+          })),
+        ),
+      );
+    }),
+  )
+  .subscribe((usersWithHobbies) => console.log(usersWithHobbies))`,
+    },
+    {
+      id: 'filterAndmergeUsersHobbies',
+      code: `// users = [{"name": "Jhon","firstName": "Trump","age": 40,"job": "Front end developer","salary": 30000},...]
+// hobbies = [{"name": "Jhon","hobbies": ["fishing, music, sport"]},...]
+const user$ = this.userService.getUsers().pipe(
+  map((users: IUser[]) => users.find((u: IUser) => u.name === 'Jhon')),
+  delay(2000),
+);
+const hobbies$ = this.userService.gethobbies();
+forkJoin(user$, hobbies$)
+  .pipe(
+    map(([u, h]: [IUser | undefined, IHobby[]]) => ({
+      ...u,
+      hobbies: h.find((hobby: IHobby) => hobby.name === u?.name),
+    })),
+  )
+  .subscribe((data) => console.log(data))`,
+    },
   ],
 };
--- src/app/pages/real-life/real-life.component.ts
+++ src/app/pages/real-life/real-life.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, OnInit, Renderer2 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { filter, map, mergeMap, Subscription, toArray } from 'rxjs';
+import { concatMap, delay, filter, forkJoin, map, mergeMap, of, Subscription, toArray } from 'rxjs';
 import { ExampleComponent } from '../../components/example/example.component';
 import { IntroductionComponent } from '../../components/introduction/introduction.component';
 import { realLife } from '../../info/realLife';
 import { IInfo, IUser } from '../../models/interfaces';
 import { IntroductionService } from '../../services/introduction.service';
 import { UserService } from '../../services/user.service';
+import { IHobby } from './../../models/interfaces';
 
 @Component({
   selector: 'app-real-life',
@@ -40,6 +41,8 @@ export class RealLifeComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     this.filterUsersMap();
     this.filterUsersMergeMapFilter();
+    this.mergeUsersHobbiesforkJoinConcatMap();
+    this.filterAndmergeUsersHobbies();
   }
 
   private filterUsersMap(): void {
@@ -63,6 +66,47 @@ export class RealLifeComponent implements OnInit, AfterViewInit {
         .subscribe((users: IUser[]) =>
           this.addConsole('filterUsersMergeMapFilter', JSON.stringify(users)),
         ),
+    );
+  }
+
+  private mergeUsersHobbiesforkJoinConcatMap(): void {
+    const users$ = this.userService.getUsers().pipe(delay(2000));
+    const hobbies$ = this.userService.gethobbies();
+    this.subscription.add(
+      forkJoin(users$, hobbies$)
+        .pipe(
+          concatMap(([u, h]: [IUser[], IHobby[]]) => {
+            return of({ users: u, hobbies: h }).pipe(
+              map((data: { users: IUser[]; hobbies: IHobby[] }) =>
+                data.users.map((user: IUser) => ({
+                  ...user,
+                  hobbies: data.hobbies.find((hobby: IHobby) => hobby.name === user.name)?.hobbies,
+                })),
+              ),
+            );
+          }),
+        )
+        .subscribe((usersWithHobbies) =>
+          this.addConsole('mergeUsersHobbiesforkJoinConcatMap', JSON.stringify(usersWithHobbies)),
+        ),
+    );
+  }
+
+  private filterAndmergeUsersHobbies(): void {
+    const user$ = this.userService.getUsers().pipe(
+      map((users: IUser[]) => users.find((u: IUser) => u.name === 'Jhon')),
+      delay(2000),
+    );
+    const hobbies$ = this.userService.gethobbies();
+    this.subscription.add(
+      forkJoin(user$, hobbies$)
+        .pipe(
+          map(([u, h]: [IUser | undefined, IHobby[]]) => ({
+            ...u,
+            hobbies: h.find((hobby: IHobby) => hobby.name === u?.name),
+          })),
+        )
+        .subscribe((data) => this.addConsole('filterAndmergeUsersHobbies', JSON.stringify(data))),
     );
   }
 
